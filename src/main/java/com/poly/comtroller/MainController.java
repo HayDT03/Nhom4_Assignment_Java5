bb78af1971b6f0a8963f102254c05aeb58b0b90b
HayDT03 <101821636+haydt21@users.noreply.github.com>
2023-06-02 20:44:14 +0700

    hay commit

--- src/main/java/com/poly/comtroller/MainController.java
+++ src/main/java/com/poly/comtroller/MainController.java
@@ -1,10 +1,10 @@
 package com.poly.comtroller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 	@GetMapping("/")
 	public String index(Model model) {
