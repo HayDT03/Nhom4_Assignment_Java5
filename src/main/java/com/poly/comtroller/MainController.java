3190d75027b59841d010d9064ad5d519822fa1bb
HayDT03 <101821636+haydt21@users.noreply.github.com>
2023-06-02 17:42:33 +0700

    hay commit
    Thêm index.jsp, MainController

---
+++ src/main/java/com/poly/comtroller/MainController.java
@@ -0,0 +1,14 @@
+package com.poly.comtroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("message", "Chào mừng bạn đến với website");
+		return "index";
+	}
+}
