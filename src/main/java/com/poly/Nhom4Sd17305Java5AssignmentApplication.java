ad77e46c0ba407a43df384b5e367839a46f15e55
haydt21 <101821636+haydt21@users.noreply.github.com>
2023-06-02 15:48:39 +0700

    Add files via upload

---
+++ src/main/java/com/poly/Nhom4Sd17305Java5AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom4Sd17305Java5AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom4Sd17305Java5AssignmentApplication.class, args);
+	}
+
+}
